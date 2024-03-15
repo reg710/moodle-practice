d187e70024347ef6644d0bf4d72594103bc658bb
reg710 <reg710@gmail.com>
2024-03-15 17:22:31 -0400

    Create upgrade process to add new field to message table

--- local/greetings/version.php
+++ local/greetings/version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_greetings';
 $plugin->release = '0.1.0';
-$plugin->version = 2024030600;
+$plugin->version = 2024031500;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
